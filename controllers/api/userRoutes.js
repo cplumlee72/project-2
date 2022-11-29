a55204d47aef58acb8fa778e26d84aa9b9fa1174
Caleb <bambini6@gmail.com>
2022-11-28 23:56:05 -0600

    added put request to manipulate existing data within the customer table in project_db

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,89 +1,105 @@
-const router = require('express').Router();
-const withAuth = require('../../utils/auth');
-const { User, Customer } = require('../../models');
+const router = require("express").Router();
+const withAuth = require("../../utils/auth");
+const { User, Customer } = require("../../models");
 // "EG: localhost/api/users/login"
-router.post('/login', async (req, res) => {
-    try {
-      const userData = await User.findOne({ where: { email: req.body.email } });
-  
-      if (!userData) {
-        res
-          .status(400)
-          .json({ message: 'Incorrect email or password, please try again' });
-        return;
-      }
-  
-      const validPassword = await userData.checkPassword(req.body.password);
-  
-      if (!validPassword) {
-        res
-          .status(400)
-          .json({ message: 'Incorrect email or password, please try again' });
-        return;
-      }
-  
-      req.session.save(() => {
-        req.session.user_id = userData.id;
-        req.session.logged_in = true;
-        
-        res.json({ user: userData, message: 'You are now logged in!' });
-      });
-  
-    } catch (err) {
-      res.status(400).json(err);
-    }
-  });
+router.post("/login", async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { email: req.body.email } });
 
-  // "EG: localhost/api/users/data"
-  router.get("/data", async (req, res) => {
-    try {
-      const customerData = await Customer.findAll({
-        order: [["name", "ASC"]],
-      });
-  
-      const customers = customerData.map((project) =>
-        project.get({ plain: true })
-      );
-  
-      // res.json(customers);
-      res.render("datapage", {
-        customers,
-        logged_in: req.session.logged_in,
-      });
-    } catch (err) {
-      res.status(500).json(err);
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: "Incorrect email or password, please try again" });
+      return;
     }
-  })
 
-  router.post('/logout', (req, res) => {
-    if (req.session.logged_in) {
-      req.session.destroy(() => {
-        res.status(204).end();
-      });
-    } else {
-      res.status(404).end();
-    }
-  });
+    const validPassword = await userData.checkPassword(req.body.password);
 
-  router.get("/data/:id", async (req, res) => {
-    try {
-      const customerData = await Customer.findAll({
-        where: {id: req.params.id},
-      });
-  
-      const customers = customerData.map((project) =>
-        project.get({ plain: true })
-      );
-  
-      // res.json(customers);
-      res.render("singleCust", {
-        customers,
-        logged_in: req.session.logged_in,
-      });
-    } catch (err) {
-      res.status(500).json(err);
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: "Incorrect email or password, please try again" });
+      return;
     }
-  })
-  
-  module.exports = router;
-  
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.json({ user: userData, message: "You are now logged in!" });
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// "EG: localhost/api/users/data"
+router.get("/data", async (req, res) => {
+  try {
+    const customerData = await Customer.findAll({
+      order: [["name", "ASC"]],
+    });
+
+    const customers = customerData.map((project) =>
+      project.get({ plain: true })
+    );
+
+    // res.json(customers);
+    res.render("datapage", {
+      customers,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post("/logout", (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+router.get("/data/:id", async (req, res) => {
+  try {
+    const customerData = await Customer.findAll({
+      where: { id: req.params.id },
+    });
+
+    const customers = customerData.map((project) =>
+      project.get({ plain: true })
+    );
+
+    // res.json(customers);
+    res.render("singleCust", {
+      customers,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.put("/data/:id", async (req, res) => {
+  try {
+    const customerData = await Customer.update(
+      {
+        name: req.body.name,
+        email: req.body.email,
+        address: req.body.address,
+        cost: req.body.cost,
+      },
+      { where: { id: req.params.id } }
+    );
+      
+    res.status(200).json(req.body);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
--- public/customerPage.js
+++ public/customerPage.js
@@ -1,46 +1,64 @@
-let editButtons = document.querySelectorAll(".editButton");
 let fields = document.querySelectorAll(".info");
 
 const editCustomer = (event) => {
-  let editButtons = document.querySelectorAll(".editButton");
+  let editButtons = event.target;
 
-  editButtons.forEach((button) => {
-    button.innerHTML = "save";
-    button.setAttribute("class", "saveButton");
-  });
-  let saveButtons = document.querySelectorAll(".saveButton");
-
-  saveButtons.forEach((button) => {
-    button.addEventListener("click", saveCustomer);
-  });
+  editButtons.innerHTML = "save";
+  editButtons.setAttribute("state", "save");
+  editButtons.classList.remove("edit");
+  editButtons.classList.add("save");
 
   fields.forEach((field) => {
     field.setAttribute("contenteditable", "true");
   });
-
 };
 
 const saveCustomer = (event) => {
-  let saveButtons = document.querySelectorAll(".saveButton");
-  saveButtons.forEach((button) => {
-    button.innerHTML = "edit";
-    button.setAttribute("class", "editButton");
-  });
-  let editButtons = document.querySelectorAll(".editButton");
-  editButtons.forEach((button) => {
-    button.addEventListener("click", editCustomer);
-  });
+  const name = document.querySelector('#namefield').innerHTML;
+  const email = document.querySelector('#emailfield').innerHTML;
+  const address = document.querySelector('#addressfield').innerHTML;
+  const cost = document.querySelector('#costfield').innerHTML;
+  console.log(JSON.stringify({name, email, address, cost}))
+  fetch(`/api/users/data/${event.target.getAttribute('id')}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({name, email, address, cost}),
+  })
+    .then((res) => console.log(res.json()))
+    .then((data) => {
+      console.log('Successful POST request:', data);
+      return data;
+    })
+    .catch((error) => {
+      console.error('Error in POST request:', error);
+    });
+
+  let saveButtons = event.target;
+
+  saveButtons.innerHTML = "edit";
+  saveButtons.setAttribute("state", "edit");
+  saveButtons.classList.remove("save");
+  saveButtons.classList.add("edit");
 
   fields.forEach((field) => {
     field.setAttribute("contenteditable", "false");
   });
+
+
 };
-let saveButtons = document.querySelectorAll(".saveButton");
 
-
-editButtons.forEach((button) => {
-  button.addEventListener("click", editCustomer);
-});
-saveButtons.forEach((button) => {
-    button.addEventListener("click", saveCustomer);
+let functionButtons = document.querySelectorAll(".functionButton");
+console.log(functionButtons);
+functionButtons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    // console.log(event.target.getAttribute("state"));
+    if (event.target.getAttribute("state") === "edit") {
+      editCustomer(event);
+    } else if (event.target.getAttribute("state") === "save") {
+      saveCustomer(event);
+    }
+    return;
   });
+});
